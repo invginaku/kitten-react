10f9b54b0adef2a68a70d0fc5540ce2ad11718c7
Nikita Ivochkin <70746527+invginaku@users.noreply.github.com>
2021-12-05 16:21:39 +0300

    Добавил Footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Header from '../Header/Header.jsx'
+import Footer from '../Footer/Footer.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
