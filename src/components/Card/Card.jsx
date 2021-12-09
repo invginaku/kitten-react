de47b8c1c35a98e647a498687ac9fe58be0bcaf2
Nikita Ivochkin <70746527+invginaku@users.noreply.github.com>
2021-12-09 23:09:29 +0300

    Добавил функцию сохранения в локальное хранилище

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,27 +2,45 @@ import React from 'react';
 
 import './Card.css';
 
-import Like from '../Like/Like'
+function Card ({file}) {
 
-const Card = ({
-                  file
-              }) => {
-
+    const [isLike, setIsLike] = React.useState( JSON.parse(localStorage.getItem('saveCard')) || false);
     const description = file.data.title;
+    const id = file.data.id;
+
+    let className = isLike ? 'like like_active' : 'like';
 
     if (file.data.preview) {
         if( file.data.preview.enabled) {
-            console.log(file.data);
             let previewImage = file.data.preview.images[0].resolutions[file.data.preview.images[0].resolutions.length - 1].url;
             previewImage = previewImage.replace(/&amp;/g,"&");
+            console.log(previewImage)
+
+            const card = {
+                id,
+                description,
+                previewImage,
+                isLike
+            };
+
+            function handleClick () {
+                setIsLike(!isLike)
+            }
+
+            if (isLike === true) {
+                localStorage.setItem('saveCard', JSON.stringify(card))
+            } else if (isLike === false) {
+                localStorage.removeItem('saveCard', JSON.stringify(card))
+            }
+
             return (
                 <div className="card">
                     <img src={previewImage} className="card__photo" alt={description}/>
-                    <Like />
+                    <button className={className} onClick={handleClick} />
                 </div>
             );
+            }
         }
-    }
     return (
         <>
         </>
